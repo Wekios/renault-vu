88a0d5ce08c3ed8662a3bd2860091b54434b1d13
diwanee <diwanee@Bojans-MacBook-Pro.local>
2018-04-12 17:02:12 +0200

    second commit, removed grid, created objects

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('.c-slick-slider').slick({
-      arrows: false
+      // arrows: false,
+      prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/shoe_story/arrow-left.png'>",
+      nextArrow:"<img class='a-right control-c next slick-next' src='../images/shoe_story/arrow-right.png'>"
     });
   });
