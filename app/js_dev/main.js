4bcaf4a7b26bf50250b6482ba9fc4ba3222a1571
diwanee <diwanee@Bojans-MacBook-Pro.local>
2018-04-17 17:17:25 +0200

    Adds font-awesome for slider arrows, news-de-la for tablet

--- app/js_dev/main.js
+++ app/js_dev/main.js
@@ -1,115 +1,115 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    // navigation \\
-  
-    var $headerNavWrap = $('.c-main-navigation--wrap'),
-        $headerNav = $('.c-main-navigation'),
-        $menuTrigger = $('.b-header__menu-trigger i'),
-        $subMenuTrigger = $('.sub__menu__trigger'),
-        $menuOpenedOverlay = $('.menu__opened__overlay'),
-        $mainContent = $('.page'),
-        $headerSearch = $('.w-search-trigger'),
-        $searchOverlay = $('.w-search-popup'),
-        $searchClose = $('.search__close'),
-        windowWidth = $(window).outerWidth();
-  
-  
-    // mobile main menu \\
-  
-    if (windowWidth < 1025) {
-  
-      $menuTrigger.on('click', function(e) {
-  
+  // navigation \\
+
+  var $headerNavWrap = $('.c-main-navigation--wrap'),
+    $headerNav = $('.c-main-navigation'),
+    $menuTrigger = $('.b-header__menu-trigger i'),
+    $subMenuTrigger = $('.sub__menu__trigger'),
+    $menuOpenedOverlay = $('.menu__opened__overlay'),
+    $mainContent = $('.page'),
+    $headerSearch = $('.w-search-trigger'),
+    $searchOverlay = $('.w-search-popup'),
+    $searchClose = $('.search__close'),
+    windowWidth = $(window).outerWidth();
+
+
+  // mobile main menu \\
+
+  if (windowWidth < 1025) {
+
+    $menuTrigger.on('click', function (e) {
+
+      e.stopPropagation();
+
+      $headerNavWrap.toggleClass('inView');
+      $headerSearch.fadeToggle(200);
+      $menuOpenedOverlay.fadeToggle(200);
+      $mainContent.toggleClass('menuOpened');
+      $('body, html').toggleClass('menuOpened');
+
+      if ($menuTrigger.hasClass('fa-bars')) {
+        $menuTrigger.removeClass('fa-bars').addClass('fa-times');
+      } else {
+        $menuTrigger.removeClass('fa-times').addClass('fa-bars');
+      }
+
+    });
+
+    $.each($subMenuTrigger, function () {
+
+      var $current = $(this);
+
+      $current.on('click', function (e) {
+
+        e.preventDefault();
         e.stopPropagation();
-  
-        $headerNavWrap.toggleClass('inView');
-        $headerSearch.fadeToggle(200);
-        $menuOpenedOverlay.fadeToggle(200);
-        $mainContent.toggleClass('menuOpened');
-        $('body, html').toggleClass('menuOpened');
-  
-        if ($menuTrigger.hasClass('fa-bars')) {
-          $menuTrigger.removeClass('fa-bars').addClass('fa-times');
+
+        var $sub = $current.next('.sub__menu');
+        var $arrow = $current.prev('i.fa');
+
+        $headerNavWrap.css('overflow-y', 'scroll');
+        $sub.slideToggle(200);
+
+        if ($arrow.hasClass('fa-chevron-down')) {
+          $arrow.removeClass('fa-chevron-down').addClass('fa-chevron-up');
         } else {
-          $menuTrigger.removeClass('fa-times').addClass('fa-bars');
+          $arrow.removeClass('fa-chevron-up').addClass('fa-chevron-down');
         }
-  
+
       });
-  
-      $.each($subMenuTrigger, function() {
-  
-        var $current = $(this);
-  
-        $current.on('click', function(e) {
-  
-          e.preventDefault();
-          e.stopPropagation();
-  
-          var $sub = $current.next('.sub__menu');
-          var $arrow = $current.prev('i.fa');
-  
-          $headerNavWrap.css('overflow-y', 'scroll');
-          $sub.slideToggle(200);
-  
-          if ($arrow.hasClass('fa-chevron-down')) {
-            $arrow.removeClass('fa-chevron-down').addClass('fa-chevron-up');
-          } else {
-            $arrow.removeClass('fa-chevron-up').addClass('fa-chevron-down');
-          }
-  
-        });
-  
-      });
-  
-    }
-  
-    // search overlay \\
-  
-    $headerSearch.on('click', function() {
-  
-      $searchOverlay.fadeIn(200);
-  
+
     });
-  
-    $searchClose.on('click', function() {
-  
-      $searchOverlay.fadeOut(200);
-  
-    });
-  
-    // desktop main menu \\
-  
-    if (windowWidth > 1024) {
-  
-      $.each($subMenuTrigger, function() {
-  
-        var $current = $(this);
-        var $li = $(this).closest('li');
-        var $sub = $li.find('.sub__menu');
-  
-        $li.on('mouseenter', function() {
-  
-          $current.addClass('active');
-          $sub.fadeIn(200);
-  
-        });
-  
-        $sub.on('mouseleave', function() {
-  
-          $sub.fadeOut(200);
-          $current.removeClass('active');
-  
-        });
-  
+
+  }
+
+  // search overlay \\
+
+  $headerSearch.on('click', function () {
+
+    $searchOverlay.fadeIn(200);
+
+  });
+
+  $searchClose.on('click', function () {
+
+    $searchOverlay.fadeOut(200);
+
+  });
+
+  // desktop main menu \\
+
+  if (windowWidth > 1024) {
+
+    $.each($subMenuTrigger, function () {
+
+      var $current = $(this);
+      var $li = $(this).closest('li');
+      var $sub = $li.find('.sub__menu');
+
+      $li.on('mouseenter', function () {
+
+        $current.addClass('active');
+        $sub.fadeIn(200);
+
       });
-  
-    }
-    
-    // Slick Slider working version
-  
-      $('.hp-slider').slick({
-        autoplay: true,
-        prevArrow:"<img class='slick-slider__arrows slick-slider__arrows-left' src='../../assets/image/arrow-left-yellow.png'>",
-        nextArrow:"<img class='slick-slider__arrows slick-slider__arrows-right' src='../../assets/image/arrow-right-yellow.png'>"    
+
+      $sub.on('mouseleave', function () {
+
+        $sub.fadeOut(200);
+        $current.removeClass('active');
+
       });
+
     });
+
+  }
+
+  // Slick Slider working version
+
+  $('.hp-slider').slick({
+    arrows: true,
+    prevArrow: '<div class="hp-slider__arrows hp-slider__arrows-left"><span class="fas fa-arrow-circle-left"></span><span class="sr-only">Prev</span></div>',
+    nextArrow: '<div class="hp-slider__arrows hp-slider__arrows-right"><span class="fas fa-arrow-circle-right"></span><span class="sr-only">Next</span></div>'
+  });
+});
