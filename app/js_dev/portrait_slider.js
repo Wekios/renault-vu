0f67a034bf2d4f5db47c531116f102a988a94fab
diwanee <diwanee@Bojans-MacBook-Pro.local>
2018-04-19 17:15:50 +0200

    finishes hp for mobile.

--- app/js_dev/portrait_slider.js
+++ app/js_dev/portrait_slider.js
@@ -12,8 +12,8 @@ const counterTotal = $('.portrait-slider__total');
 portraitSlider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
   //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
   let i = (currentSlide ? currentSlide : 0) + 1;
-  counterCurrent.text(i);
-  counterTotal.text(" / " + slick.slideCount);
+  counterCurrent.text("0" + i);
+  counterTotal.text(" / " + "0" + slick.slideCount);
   if(slick.slideCount == null ) {
     slick.slideCount = 5;
   };
@@ -35,5 +35,29 @@ $('.portrait-slider').slick({
     infinite: true,
     prevArrow: $(".portrait-slider__arrows-left"),
     nextArrow: $(".portrait-slider__arrows-right")
+  });
+
+  $('.les-plus-slider').slick({
+    arrows: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      // {
+      //   breakpoint: 1024,
+      //   settings: {
+      //     slidesToShow: 3,
+      //     slidesToScroll: 3,
+      //     infinite: true,
+      //     dots: true
+      //   }
+      // },
+      {
+        breakpoint: 641,
+        settings: "unslick"
+      }
+    ],
+    prevArrow: $(".les-plus-slider__arrows-left"),
+    nextArrow: $(".les-plus-slider__arrows-right")
   });
 });
