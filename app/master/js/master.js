27df0dbcb21305f2b1d10df6d7ade33636585c41
Veljko Blagojevic <veljkod.blagojevic@gmail.com>
2018-05-01 12:27:34 +0200

    tablet finished, layout, not pixel perfect

--- app/master/js/master.js
+++ app/master/js/master.js
@@ -3259,6 +3259,45 @@ $('.portrait-slider').slick({
   // });
 });
 
+$(document).ready(function () {
+
+// Const and Lets
+
+const qaSlider = $('.qa-slider');
+let counterCurrent = $('.qa-slider__current');
+let counterTotal = $('.qa-slider__total');
+
+// Counter Function
+
+qaSlider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+  let i = (currentSlide ? currentSlide : 0) + 1;
+  counterCurrent.text("0" + i);
+  counterTotal.text(" / " + "0" + slick.slideCount);
+  if(slick.slideCount == null ) {
+    slick.slideCount = 3;
+  };
+});
+
+
+// Slider Settings
+
+qaSlider.slick({
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  speed: 400,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnHover: true,
+  infinite: true,
+  dots: false,
+  prevArrow: $(".qa-slider__arrows-left"),
+  nextArrow: $(".qa-slider__arrows-right")
+    });
+});
+
 (function ($) {
   // Diaporama trigger \\
 
