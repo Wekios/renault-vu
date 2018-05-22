d8812ec4e05b4193de9038ed1d517485aeef9aca
diwanee <diwanee@Bojans-MacBook-Pro.local>
2018-05-22 16:28:33 +0200

    update and done

--- app/js_dev/main.js
+++ app/js_dev/main.js
@@ -110,12 +110,6 @@ $(document).ready(function () {
         $current.addClass('active');
         $sub.fadeIn(200);
 
-        if($current.hasClass('active')) {
-          $sub.fadeIn(200);
-        } else {
-          sub.fadeOut(200);
-        }
-
       });
 
       $sub.on('mouseleave', function () {
